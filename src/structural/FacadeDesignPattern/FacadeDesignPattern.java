407ae17901d13b1701a534ff05af4747b7f07ad1
Manish <bhatia_manish@outlook.com>
2021-01-02 00:48:00 +0530

    Initial commit

---
+++ src/structural/FacadeDesignPattern/FacadeDesignPattern.java
@@ -0,0 +1,9 @@
+package structural.FacadeDesignPattern;
+
+/**
+ * Definition:- Facade design pattern provides a simple easy to use interface over a large sophisticated body of code. Many different services
+ * or mechanisms might be working together but user might just want some simple functionality which uses all these mechanisms/services so we expose
+ * this functionality through an interface
+ */
+public class FacadeDesignPattern {
+}
